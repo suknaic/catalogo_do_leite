6bea9c7b7a8ab83e5c8f5356464a2ef9ab6cdcd3
Felipe Suknaic <fsuknaic@hotmail.com>
2021-03-16 15:09:40 -0500

    configuracao do banco de dados

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,3 +1,10 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+createConnection({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'varejo360',
+});
