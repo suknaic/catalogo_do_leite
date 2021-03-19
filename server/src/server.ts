741f1640881e127d4421e40c9ba13088d686807d
Felipe Suknaic <fsuknaic@hotmail.com>
2021-03-18 22:47:46 -0500

    add paginacao

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { pagination } from 'typeorm-pagination';
 
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(pagination);
 app.use(routes);
 
 app.listen(3333, () => console.log('server started on por 3333'));
--- server/src/services/PesquisarProduto.ts
+++ server/src/services/PesquisarProduto.ts
@@ -3,18 +3,21 @@ import { getRepository, Like } from 'typeorm';
 import Produto from '../database/model/Produto';
 
 export class PesquisarProduto {
-  public async execulte(value: string): Promise<Produto[]> {
+  public async execulte(value: string): Promise<any> {
     try {
       const ProdutoRepository = getRepository(Produto);
 
       if (!!value) {
-        const resultado = await ProdutoRepository.find({
-          where: [{ nome: Like(`%${value}%`) }, { codigo: Like(`${value}%`) }],
-        });
+        const resultado = await ProdutoRepository.createQueryBuilder()
+          .where([{ nome: Like(`%${value}%`) }, { codigo: Like(`${value}%`) }])
+          .paginate(10);
+
         return resultado;
       }
 
-      const resultado = await ProdutoRepository.find();
+      const resultado = await ProdutoRepository.createQueryBuilder(
+        'produtos',
+      ).paginate(10);
       return resultado;
     } catch (error) {
       console.log(error);
